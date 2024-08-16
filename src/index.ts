13eb966a032b2f05888429dd1fbaa475bab5d861
Christian Sparks <csparks@cloudflare.com>
2024-08-16 13:45:02 -0500

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -13,10 +13,6 @@
 
 export default {
 	async fetch(request, env, ctx): Promise<Response> {
-		return new Response('Helaskkljllkjjjkljkklja
-				    
-				    
-				    
-				    klkljlkjlasdfjkjjaaaaasdfdjlklasdfjklasdfkjklalksjtest buiklddfhkkasljlkjjkldkfakksdklflko Wasdforld5!');
+		return new Response('Helaskkljllkjjjkljkkljaklkljlkjlasdfjkjjaaaaasdfdjlklasdfjklasdfkjklalksjtest buiklddfhkkasljlkjjkldkfakksdklflko Wasdforld5!');
 	},
 } satisfies ExportedHandler<Env>;
